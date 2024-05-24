cbea10f7635bab18da0d9db134fe81db564d7a6f
Baldo <barrosoub@protonmail.com>
2024-05-23 22:32:34 -0600

    A form was added

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+export default function TodoList() {
+  return (
+    <>
+     <h2>Server Actions Demo</h2>
+        <div>
+          <form action="#" method="POST">
+            <div>
+              <label htmlFor="todo">Todo</label>
+              <div>
+                <input id="todo" name="text" type="text"
+                  placeholder="What needs to be done?"
+                  required
+                />
+              </div>
+            </div>
+            <div>
+              <button type="submit"> Add Todo</button>
+            </div>
+          </form>
+        </div>
+    </>
+  );
+}
